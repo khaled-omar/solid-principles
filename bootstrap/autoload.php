040ba2e82d9808cab382642567fe3a29bce26fe1
khaled omar abdelmoneem <komar@arkdev.net>
2018-11-19 00:07:15 +0200

    Update autoload function.

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -2,7 +2,7 @@
 // Autoload function
 function __autoload($class) {
 	// Convert namespace to full file path.
-	$class =  str_replace('\\', '/', $class) . '.php';
-	$class = str_replace('App/', '', $class);
+	$class = str_replace('App/', '', str_replace('\\', '/', $class) . '.php');
+	// Require class ;
 	require_once($class);
 }
